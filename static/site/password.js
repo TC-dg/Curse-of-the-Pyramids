a4abb95dc2d243c3fc88f25b5e127fcc2e00edd1
pandayoooo <yoyoyandjoy419@gmail.com>
2022-03-05 15:12:20 +0800

    a

--- static/site/password.js
+++ static/site/password.js
@@ -2,18 +2,31 @@ window.onload=function(){
     login();
 }
 function castspell() {
-    const spell = document.getElementById("spell").value;
-    alert(spell);
-    $(".blackk").css('opacity', '1');
-    showDialog();
-    //window.location.href="../../index.html";
+    const spellinput = document.getElementById("spell").value;    
+    $.getJSON("./spell.json", function (spell) {
+        $(".blackk").css('opacity', '1');
+        document.getElementById("blackk").style.zIndex = 3;
+        for (let i = 0; i < Object.keys(spell.spell).length; i++) {
+            if (spellinput == spell.spell[i].name) {//right
+            document.getElementById("dialog").style.backgroundColor = "#FFE8C2";
+            document.getElementById("dialog").style.color = "#000000";
+            $("#msg").html("密語正確"+"<br>"+spell.spell[i].msg);
+            showDialog();   
+            } else {//wrong
+            showDialog();   
+            }            
+        }
+    });   
 }
-
 function showDialog(){
     dialog.style.display="block";
 }
 function hideDialog(){
+    document.getElementById("blackk").style.zIndex = 1; 
     dialog.style.display="none";
+    document.getElementById("dialog").style.backgroundColor = "#700000";
+    document.getElementById("dialog").style.color = "#FFFFFF";
+    document.getElementById("msg").textContent ="詛咒錯誤";
     $(".blackk").css('opacity', '0');
 }
 
