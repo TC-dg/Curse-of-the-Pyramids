549ee5aab2e7cdbe2978bc50b640f26d43330a3c
pandayoooo <yoyoyandjoy419@gmail.com>
2022-03-07 20:49:25 +0800

    b

--- index.js
+++ index.js
@@ -1,25 +1,109 @@
-/*const fs = require('fs');
-const jsonData= require('./static/data.json'); 
-console.log(jsonData);
-let newdata = { 
-    "teamname":"panda",
-    "curse1":0,
-    "curse2":0,
-    "curse3":0,
-    "curse4":0,
-    "curse5":0   
-};
-let newjson = JSON.stringify(newdata, null, '\t');
-fs.writeFileSync('./static/data.json', newjson);*/
-
 window.onload=function(){    
     login();
 }
+function castspell() {
+    const spellinput = document.getElementById("spell").value;    
+    $.getJSON("static/spell.json", function (spell) {
+        $(".blackk").css('opacity', '1');
+        document.getElementById("blackk").style.zIndex = 3;
+        for (let i = 0; i < Object.keys(spell.spell).length; i++) {
+            if (spellinput == spell.spell[i].name) {//right
+            document.getElementById("dialog").style.backgroundColor = "#FFE8C2";
+            document.getElementById("dialog").style.color = "#000000";
+            $("#msg").html("密語正確"+"<br>"+spell.spell[i].msg);
+            showDialog();            
+            doit(spellinput);
+            } else {//wrong
+            showDialog();   
+            }            
+        }
+    });   
+}
+function password(){
+    pswdreact.style.display="block";
+    cursereact.style.display="none";
+    bkpkreact.style.display="none";
+    settingreact.style.display="none";
+    clearother3("curse","bpack","setting");
+    document.getElementsByClassName("passwd")[0].setAttribute("style","border-color: #33ccff;border-style:solid;border-radius: 10px;border-width: 2px;background-color: rgba(0, 0, 0, 0.3);");
+}
+function curse(){
+    pswdreact.style.display="none";
+    bkpkreact.style.display="none";
+    settingreact.style.display="none";
+    cursereact.style.display="block";
+    clearother3("passwd","bpack","setting");
+    document.getElementsByClassName("curse")[0].setAttribute("style","border-color: #33ccff;border-style:solid;border-radius: 10px;border-width: 2px;background-color: rgba(0, 0, 0, 0.3);");
+    checkeye();
+}
+function backpack(){
+    pswdreact.style.display="none";
+    cursereact.style.display="none";
+    bkpkreact.style.display="block";
+    settingreact.style.display="none";
+    clearother3("passwd","curse","setting");
+    document.getElementsByClassName("bpack")[0].setAttribute("style","border-color: #33ccff;border-style:solid;border-radius: 10px;border-width: 2px;background-color: rgba(0, 0, 0, 0.3);");
+    //tt();
+}
+function settings(){
+    pswdreact.style.display="none";
+    cursereact.style.display="none";
+    bkpkreact.style.display="none";
+    settingreact.style.display="block";
+    clearother3("passwd","curse","bpack");
+    document.getElementsByClassName("setting")[0].setAttribute("style","border-color: #33ccff;border-style:solid;border-radius: 10px;border-width: 2px;background-color: rgba(0, 0, 0, 0.3);");
+}
+function showDialog(){
+    dialog.style.display="block";
+}
+function hideDialog(){
+    dialog.style.display="none";
+    document.getElementById("blackk").style.zIndex = 1; 
+    document.getElementById("dialog").style.backgroundColor = "#700000";
+    document.getElementById("dialog").style.color = "#FFFFFF";
+    document.getElementById("msg").textContent ="詛咒錯誤";
+    $(".blackk").css('opacity', '0');
+}
+function firstlogin(){
+    teannameinput.style.display="none";
+}
+function login(){
+    //document.getElementById("teamname").textContent = "隊名:"+document.getElementById("login").value;
+    $.getJSON("static/data.json", function (data) {
+        //data.teamname = document.getElementById("login").value;
+        return $("#teamname").html("隊名:"+data.teamname);
+    });    
+    pswdreact.style.display="none";
+    cursereact.style.display="none";
+    bkpkreact.style.display="none";
+    settingreact.style.display="none";
+}
+function clearother3(a,b,c){    
+    document.getElementsByClassName(a)[0].setAttribute("style"," ");
+    document.getElementsByClassName(b)[0].setAttribute("style"," ");
+    document.getElementsByClassName(c)[0].setAttribute("style"," ");
+}
+async function checkeye() {
+    var eyeimg=document.getElementById("eye");
+    eyeimg.src="static/image/eye5.png";    
+    await sleep(2000);
+    eyeimg.src="static/image/eye0.png";    
+}
+function tt() {    
+    var title = document.getElementById("item2title");
+    var jpg = document.getElementById("item2jpg");
+    jpg.src="static/image/panda2.jpg";
+    title.innerText="熊貓變了";    
+}
 
-function login(){    
-    $.getJSON("./static/data.json", function (data) {
-        data.teamname = document.getElementById("login").value;
-        //document.getElementById("teamname").textContent = "隊名:"+document.getElementById("login").value;
-        document.getElementById("teamname").textContent = "隊名:"+data.teamname;
-    });
+function doit(spell) {
+    if (spell =="p2") {        
+        var title = document.getElementById("item2title");
+        var jpg = document.getElementById("item2jpg");
+        jpg.src="static/image/panda2.jpg";
+        title.innerText="熊貓變了";    
+    }
+}
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
