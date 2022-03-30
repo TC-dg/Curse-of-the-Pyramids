b733c0785f62128947bd55698162d1781625bd6d
pandayoooo <yoyoyandjoy419@gmail.com>
2022-03-30 16:28:17 +0800

    0333

--- index.js
+++ index.js
@@ -7,61 +7,84 @@ var cursestat = {
     'now': '00000',
 }
 
-/*window.onload = function () {
-    active();
-}*/
 document.addEventListener("DOMContentLoaded", function (event) {
     active();
 });
 
 function active() {
-    document.getElementById("passwd").style.animationPlayState = "paused";
-    document.getElementById("curse").style.animationPlayState = "paused";
-    document.getElementById("bpack").style.animationPlayState = "paused";
-    document.getElementById("setting").style.animationPlayState = "paused";
     pswdreact.style.display = "none";
     cursereact.style.display = "none";
     bkpkreact.style.display = "none";
     settingreact.style.display = "none";
+    document.getElementById("passwd").style.animationPlayState = "paused";
+    document.getElementById("curse").style.animationPlayState = "paused";
+    document.getElementById("bpack").style.animationPlayState = "paused";
+    document.getElementById("setting").style.animationPlayState = "paused";
     document.getElementById("bott").style.pointerEvents = "none";
 }
 
 function firstlogin() {
-    if (document.getElementById("login").value != "") {
-        const teamname = document.getElementById("login").value;
-        localStorage.setItem('teamname', teamname);
-        $("#teamname").html("隊名:" + localStorage.getItem('teamname'));
+    if (
+        /*document.getElementById("teamID").value != "" && document.getElementById("teamNAME").value != ""*/
+        1) {
+        //enter something
+        axios.post("https://192.168.1.176/api/login", {
+            id: document.getElementById("teamID").value,
+            teamname: document.getElementById("teamNAME").value //post -> team_name
+        }).then((response) => {
+            const {
+                data
+            } = response;
+            console.log(data);
+            if (data.response == "success") {
+                //yes;
+            } else {
+                //no;
+                /*alert("隊伍ID錯誤 請與主辦方確認");
+                return 0;*/
+            }
+        }).catch((error) => {
+            const {
+                data
+            } = error.response;
+            console.log(data);
+        });
         teannameinput.style.display = "none";
+        document.getElementById("passwd").style.animationPlayState = "running";
+        document.getElementById("curse").style.animationPlayState = "running";
+        document.getElementById("bpack").style.animationPlayState = "running";
+        document.getElementById("setting").style.animationPlayState = "running";
+        document.getElementById("bott").style.pointerEvents = "auto";
+        password();
+        const teamname = document.getElementById("teamNAME").value;
+        localStorage.setItem('teamname', teamname);
+        $("#teamname_display").html("隊名:" + localStorage.getItem('teamname'));
+        localStorage.setItem('LScursestatus', JSON.stringify(cursestat));
+        cu = JSON.parse(localStorage.getItem('LScursestatus'));
+        tmpnow = "00000";
     } else {
-        /*alert("請輸入隊名");
+        /*alert("請輸入正確的隊伍ID及隊名");
         return 0;*/
     }
-    document.getElementById("passwd").style.animationPlayState = "running";
-    document.getElementById("curse").style.animationPlayState = "running";
-    document.getElementById("bpack").style.animationPlayState = "running";
-    document.getElementById("setting").style.animationPlayState = "running";
-    document.getElementById("bott").style.pointerEvents = "auto";
-    password();
-    localStorage.setItem('LScursestatus', JSON.stringify(cursestat));
-    cu = JSON.parse(localStorage.getItem('LScursestatus'));
-    tmpnow = "00000";
 }
-
 
 function castspell() {
     const spellinput = document.getElementById("spell").value;
+
     $.getJSON("static/spell.json", function (spell) {
         $(".blackk").css('opacity', '1');
         document.getElementById("blackk").style.zIndex = 3;
         for (let i = 0; i < Object.keys(spell.spell).length; i++) {
-            if (spellinput == spell.spell[i].name) { //right
-                document.getElementById("spellconv").style.backgroundColor = "#FFE8C2";
+            if (spellinput == spell.spell[i].name) {
+                //right
+                document.getElementById("spellconv").style.backgroundColor = "#fced6e";
                 document.getElementById("spellconv").style.color = "#000000";
                 $("#msg").html("密語正確" + "<br>" + spell.spell[i].msg);
                 showconv(document.getElementById("spellconv"));
                 doit(spell.spell[i]);
                 break;
-            } else { //wrong
+            } else {
+                //wrong
                 $("#msg").html("密語錯誤");
                 showconv(document.getElementById("spellconv"));
             }
@@ -74,8 +97,8 @@ function password() {
     cursereact.style.display = "none";
     bkpkreact.style.display = "none";
     settingreact.style.display = "none";
-    clearother3("curse", "bpack", "setting");
-    document.getElementsByClassName("passwd")[0].setAttribute("style", "border-color: #33ccff;border-style:solid;border-radius: 10px;border-width: 3px;background-color: rgba(0, 0, 0, 0.3);");
+    clearother3(1, 2, 3);
+    document.getElementById("passwd").setAttribute("style", "border-color: rgb(27, 228, 255);border-style:solid;border-radius: 10px;border-width: 3px;background-color: rgba(0, 0, 0, 0.3);");
 }
 
 function curse() {
@@ -83,8 +106,8 @@ function curse() {
     bkpkreact.style.display = "none";
     settingreact.style.display = "none";
     cursereact.style.display = "block";
-    clearother3("passwd", "bpack", "setting");
-    document.getElementsByClassName("curse")[0].setAttribute("style", "border-color: #33ccff;border-style:solid;border-radius: 10px;border-width: 3px;background-color: rgba(0, 0, 0, 0.3);");
+    clearother3(0, 2, 3);
+    document.getElementById("curse").setAttribute("style", "border-color: rgb(27, 228, 255);border-style:solid;border-radius: 10px;border-width: 3px;background-color: rgba(0, 0, 0, 0.3);");
     checkeye();
 }
 
@@ -93,8 +116,8 @@ function backpack() {
     cursereact.style.display = "none";
     bkpkreact.style.display = "block";
     settingreact.style.display = "none";
-    clearother3("passwd", "curse", "setting");
-    document.getElementsByClassName("bpack")[0].setAttribute("style", "border-color: #33ccff;border-style:solid;border-radius: 10px;border-width: 3px;background-color: rgba(0, 0, 0, 0.3);");
+    clearother3(0, 1, 3);
+    document.getElementById("bpack").setAttribute("style", "border-color: rgb(27, 228, 255);border-style:solid;border-radius: 10px;border-width: 3px;background-color: rgba(0, 0, 0, 0.3);");
     //tt();
 }
 
@@ -103,8 +126,8 @@ function settings() {
     cursereact.style.display = "none";
     bkpkreact.style.display = "none";
     settingreact.style.display = "block";
-    clearother3("passwd", "curse", "bpack");
-    document.getElementsByClassName("setting")[0].setAttribute("style", "border-color: #33ccff;border-style:solid;border-radius: 10px;border-width: 3px;background-color: rgba(0, 0, 0, 0.3);");
+    clearother3(0, 1, 2);
+    document.getElementById("setting").setAttribute("style", "border-color: rgb(27, 228, 255);border-style:solid;border-radius: 10px;border-width: 3px;background-color: rgba(0, 0, 0, 0.3);");
 }
 
 function showconv(c) {
@@ -112,9 +135,9 @@ function showconv(c) {
 }
 
 function clearother3(a, b, c) {
-    document.getElementsByClassName(a)[0].setAttribute("style", " ");
-    document.getElementsByClassName(b)[0].setAttribute("style", " ");
-    document.getElementsByClassName(c)[0].setAttribute("style", " ");
+    document.getElementsByClassName("icon")[a].setAttribute("style", " ");
+    document.getElementsByClassName("icon")[b].setAttribute("style", " ");
+    document.getElementsByClassName("icon")[c].setAttribute("style", " ");
 }
 
 function hideconv() {
@@ -130,12 +153,12 @@ function hideconv() {
     $(".blackk").css('opacity', '0');
 }
 
-async function checkeye() {
+function checkeye() {
     var eyeimg = document.getElementById("eye");
     eyeimg.src = "static/image/eye" + tmpnow + ".png";
 }
 
-async function cur1() {
+function cur1() {
     $(".blackk").css('opacity', '1');
     document.getElementById("blackk").style.zIndex = 3;
     var cursemsgtitle = document.getElementById("cursemsgtitle");
@@ -145,7 +168,7 @@ async function cur1() {
     showconv(document.getElementById("curseconv"));
 }
 
-async function cur2() {
+function cur2() {
     $(".blackk").css('opacity', '1');
     document.getElementById("blackk").style.zIndex = 3;
     var cursemsgtitle = document.getElementById("cursemsgtitle");
@@ -155,7 +178,7 @@ async function cur2() {
     showconv(document.getElementById("curseconv"));
 }
 
-async function cur3() {
+function cur3() {
     $(".blackk").css('opacity', '1');
     document.getElementById("blackk").style.zIndex = 3;
     var cursemsgtitle = document.getElementById("cursemsgtitle");
@@ -165,7 +188,7 @@ async function cur3() {
     showconv(document.getElementById("curseconv"));
 }
 
-async function cur4() {
+function cur4() {
     $(".blackk").css('opacity', '1');
     document.getElementById("blackk").style.zIndex = 3;
     var cursemsgtitle = document.getElementById("cursemsgtitle");
@@ -175,7 +198,7 @@ async function cur4() {
     showconv(document.getElementById("curseconv"));
 }
 
-async function cur5() {
+function cur5() {
     $(".blackk").css('opacity', '1');
     document.getElementById("blackk").style.zIndex = 3;
     var cursemsgtitle = document.getElementById("cursemsgtitle");
@@ -186,42 +209,15 @@ async function cur5() {
 }
 
 function doit(spell) {
-    if (spell.name == "p2") {
-        itemnum = 2;
-        itemnumbertitle = "item" + itemnum + "title";
-        itemnumberjpg = "item" + itemnum + "jpg";
-        itemandnumber = "item" + itemnum;
-        var iframe = document.getElementById("packframe");
-        var elmnt = iframe.contentWindow.document.getElementById(itemandnumber);
-        var title = iframe.contentWindow.document.getElementById(itemnumbertitle);
-        var jpg = iframe.contentWindow.document.getElementById(itemnumberjpg);
-        jpg.src = "static/image/panda2.jpg";
-        title.innerText = "熊貓變了";
-    }
-    if (spell.name == "c1") {
-        cursestat.curse1 = '1';
-        cursestat.now = (cursestat.curse1).toString() + (cursestat.curse2).toString() + (cursestat.curse3).toString() + (cursestat.curse4).toString() + (cursestat.curse5).toString();
-    }
-    if (spell.name == "c2") {
-        cursestat.curse2 = '1';
-        cursestat.now = (cursestat.curse1).toString() + (cursestat.curse2).toString() + (cursestat.curse3).toString() + (cursestat.curse4).toString() + (cursestat.curse5).toString();
-    }
-    if (spell.name == "c3") {
-        cursestat.curse3 = '1';
-        cursestat.now = (cursestat.curse1).toString() + (cursestat.curse2).toString() + (cursestat.curse3).toString() + (cursestat.curse4).toString() + (cursestat.curse5).toString();
-    }
-    if (spell.name == "c4") {
-        cursestat.curse4 = '1';
-        cursestat.now = (cursestat.curse1).toString() + (cursestat.curse2).toString() + (cursestat.curse3).toString() + (cursestat.curse4).toString() + (cursestat.curse5).toString();
-    }
     if (spell.name == "c5") {
-        if (document.getElementById("eye").src == "static/image/eye11110.png") {
-            cursestat.curse5 = '1';
-            cursestat.now = (cursestat.curse1).toString() + (cursestat.curse2).toString() + (cursestat.curse3).toString() + (cursestat.curse4).toString() + (cursestat.curse5).toString();
+        if (eval(spell.condition)) {
+            eval(spell.action);
         } else {
             $("#msg").html("條件不足");
         }
     }
+
+    eval(spell.action);
     localStorage.setItem('LScursestatus', JSON.stringify(cursestat));
     cu = JSON.parse(localStorage.getItem('LScursestatus'));
     console.log(cu);
@@ -233,83 +229,85 @@ function sleep(ms) {
 }
 
 function cameraon() {
-    let scanner = new Instascan.Scanner({
-        video: document.getElementById('preview'), // 預覽
-        continuous: true, // 連續掃描
-        mirror: false,
-        refractoryPeriod: 10000,
-        backgroundScan: false,
-        scanPeriod: 10,
-        facingMode: {
-            exact: "environment"
-        }
-    });
-    scanner.addListener('scan', function (content) {
-        console.log(content);
-        getConfirmation();
+    /*
+        let scanner = new Instascan.Scanner({
+            video: document.getElementById('preview'), // 預覽
+            continuous: true, // 連續掃描
+            mirror: false,
+            refractoryPeriod: 10000,
+            backgroundScan: false,
+            scanPeriod: 10,
+            facingMode: {
+                exact: "environment"
+            }
+        });
+        scanner.addListener('scan', function (content) {
+            console.log(content);
+            getConfirmation();
 
-        function getConfirmation() {
-            var retVal = confirm("掃描結果：" + content); // 掃描結果顯示
-            if (retVal == true) {
-                var checkurl =
-                    /^((https?|http?|file):\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/; // 檢查是否為網址
-                if (checkurl.test(content)) {
-                    window.open(content, "_self"); // 開啟網址
+            function getConfirmation() {
+                var retVal = confirm("掃描結果：" + content); // 掃描結果顯示
+                if (retVal == true) {
+                    var checkurl =
+                        /^((https?|http?|file):\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/; // 檢查是否為網址
+                    if (checkurl.test(content)) {
+                        window.open(content, "_self"); // 開啟網址
+                    }
+                    return true;
+                } else {
+                    return false;
                 }
-                return true;
-            } else {
-                return false;
             }
-        }
-    });
+        });
 
-    Instascan.Camera.getCameras().then(function (cameras) {
-        /*$('.camera_list li').empty();
-        if (cameras.length > 0) {
-            cameras.forEach(function (c, i) {
-                camera_name = c.name;
-                camera_id = i;
-                $('.camera_list').append('<li id=' + i + ' class="items"><i class="fa fa-camera fa-lg" aria-hidden="true"></i> 鏡頭 ' + (camera_id + 1) + ' </li>');
-            });
+        Instascan.Camera.getCameras().then(function (cameras) {
+            /*$('.camera_list li').empty();
+            if (cameras.length > 0) {
+                cameras.forEach(function (c, i) {
+                    camera_name = c.name;
+                    camera_id = i;
+                    $('.camera_list').append('<li id=' + i + ' class="items"><i class="fa fa-camera fa-lg" aria-hidden="true"></i> 鏡頭 ' + (camera_id + 1) + ' </li>');
+                });
 
-            $(".camera_list li").click(function () {
-                active = $(this).attr("id");
-                scanner.start(cameras[active]);
-            });
-            if (cameras.length >= 2) {
-                scanner.start(cameras[1]);
+                $(".camera_list li").click(function () {
+                    active = $(this).attr("id");
+                    scanner.start(cameras[active]);
+                });
+                if (cameras.length >= 2) {
+                    scanner.start(cameras[1]);
+                } else {
+                    scanner.start(cameras[0]);
+                }
             } else {
+                alert("找不到相機...請確認允許使用相機");
+            }*/
+    /*
+            if (cameras.length > 1) {
+                scanner.start(cameras[1]); // [0] 前鏡頭 [1] 後鏡頭 
+            } else if (cameras.length > 0) {
                 scanner.start(cameras[0]);
+            } else {
+                console.error('沒有找到相機');
             }
-        } else {
-            alert("找不到相機...請確認允許使用相機");
-        }*/
-        if (cameras.length > 1) {
-            scanner.start(cameras[1]); // [0] 前鏡頭 [1] 後鏡頭 
-        } else if (cameras.length > 0) {
-            scanner.start(cameras[0]);
-        } else {
-            console.error('沒有找到相機');
-        }
-    }).catch(function (e) {
-        console.error(e);
-    });
-    const pre = document.getElementById("preview");
-    const cam = document.getElementById("camera");
-    cam.style.display = "none";
-    pre.style.display = "block";
-    pre.style.zIndex = "10";
-    pre.style.width = "40vw";
-
-    setTimeout(function () {
+        }).catch(function (e) {
+            console.error(e);
+        });
         const pre = document.getElementById("preview");
         const cam = document.getElementById("camera");
-        scanner.stop();
-        cam.style.display = "block";
-        pre.style.display = "none";
-        pre.style.zIndex = "0";
-        pre.style.width = "0vw";
-    }, 10000);
+        cam.style.display = "none";
+        pre.style.display = "block";
+        pre.style.zIndex = "10";
+        pre.style.width = "40vw";
+
+        setTimeout(function () {
+            const pre = document.getElementById("preview");
+            const cam = document.getElementById("camera");
+            scanner.stop();
+            cam.style.display = "block";
+            pre.style.display = "none";
+            pre.style.zIndex = "0";
+            pre.style.width = "0vw";
+        }, 10000);*/
 }
 
 
@@ -386,4 +384,20 @@ function set3() {
     $(".blackk").css('opacity', '1');
     document.getElementById("blackk").style.zIndex = 3;
     showconv(document.getElementById("creditconv"));
+}
+
+function sanimation() {
+    let elem = document.getElementById("sanbar");
+    let stepValue = 0;
+    let id = setInterval(frame, 100);
+
+    function frame() {
+        if (stepValue >= 105) {
+            stepValue = 0;
+        } else {
+            elem.style.width = (stepValue + 1) + "%";
+            elem.innerHTML = "???%";
+            stepValue = (stepValue + 1);
+        }
+    }
 }
